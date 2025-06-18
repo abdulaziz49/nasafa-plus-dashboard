95a916dac3753c67e0b8d6a21c6d23f758573371
SulimanDahan <suliman2015dahan@gmail.com>
2025-06-18 23:12:52 +0300

    1-select component 2-toggle componet
    were added

---
+++ src/components/inputs/toggle.tsx
@@ -0,0 +1,42 @@
+interface ToggleType {
+    name: string,
+    withCheckMark: boolean
+}
+
+const Toggle = ({name, withCheckMark}: ToggleType) => {
+    return (
+        <label className="toggle text-base-content">
+            <input name={name} type="checkbox"/>
+            {
+                withCheckMark ?? <>
+                    <svg aria-label="enabled" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <g
+                            strokeLinejoin="round"
+                            strokeLinecap="round"
+                            strokeWidth="4"
+                            fill="none"
+                            stroke="currentColor"
+                        >
+                            <path d="M20 6 9 17l-5-5"></path>
+                        </g>
+                    </svg>
+                    <svg
+                        aria-label="disabled"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <path d="M18 6 6 18"/>
+                        <path d="m6 6 12 12"/>
+                    </svg>
+                </>
+            }
+        </label>
+    )
+}
+
+export default Toggle
