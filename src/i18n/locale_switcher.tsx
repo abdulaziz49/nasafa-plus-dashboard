3eb13e4dedf71f7e5a05489fc4490511a1489090
SulimanDahan <suliman2015dahan@gmail.com>
2025-06-23 02:34:46 +0300

    first try to add google fonts

--- src/i18n/locale_switcher.tsx
+++ src/i18n/locale_switcher.tsx
@@ -1,23 +1,42 @@
 // src/i18n/LocaleSwitcher.tsx
 
-import { useTranslation } from "react-i18next";
+import {useTranslation} from "react-i18next";
 import Select from "../components/inputs/select.tsx";
 import supportedLngs from "./supported_langs.ts";
+import {type ChangeEventHandler, type ReactElement, useEffect} from "react";
 
-export default function LocaleSwitcher() {
-    const { t, i18n } = useTranslation();
+
+const checkStoredLang = (fallbackLang: string): string => {
+    try {
+        const storedLang = localStorage.getItem("lang");
+        return storedLang || fallbackLang; // Use storedLang if it exists, otherwise use fallbackLang
+    } catch {
+        localStorage.setItem("lang", fallbackLang)
+        return fallbackLang;
+    }
+};
+
+export default function LocaleSwitcher(): ReactElement {
+    const {t, i18n} = useTranslation();
+
+    useEffect(() => {
+        i18n.changeLanguage(checkStoredLang(i18n.language))
+        console.log(i18n.language)
+    }, []);
+
+    const selectChangeEvent: ChangeEventHandler<HTMLSelectElement> = async (e) => {
+        await i18n.changeLanguage(e.target.value)
+        localStorage.setItem("lang", e.target.value)
+    }
 
     return (
-        <div className="...">
-            <div className="...">
-                <Select withLabel={true} labelText={t('login-lang-switch')} onChange={(e) => i18n.changeLanguage(e.target.value)}>
-                    {Object.entries(supportedLngs).map(([code, name]) => (
-                        <option value={code} key={code}>
-                            {name}
-                        </option>
-                    ))}
-                </Select>
-            </div>
-        </div>
+        <Select withLabel={true} labelText={t('login-lang-switch')} defaultValue={i18n.language}
+                onChange={selectChangeEvent}>
+            {Object.entries(supportedLngs).map(([code, name]) => (
+                <option value={code} key={code}>
+                    {name}
+                </option>
+            ))}
+        </Select>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import {createRoot} from 'react-dom/client'
-import './index.css'
+import './assets/css/index.css'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
 // import PageDirectionController from "./controllers/utils/page_direction_controller.tsx";
 import i18n from  "./i18n/i18n.ts"
 import {I18nextProvider} from "react-i18next";
+// import './assets/css/fonts.css'
 
 createRoot(document.getElementById('root')!).render(
     <>
