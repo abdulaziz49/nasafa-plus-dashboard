e405b705b14f7ddf389842e33629cf5f1d52ea07
SulimanDahan <suliman2015dahan@gmail.com>
2025-07-05 19:02:30 +0300

    prevent basicSsl() from execute on production

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,35 @@
-import {defineConfig} from 'vite'
+// import {defineConfig} from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// // import mkcert from 'vite-plugin-mkcert'
+// import basicSsl from '@vitejs/plugin-basic-ssl'
+// // import pkg from './package.json' with {type: "json"}
+//
+// export default defineConfig({
+//     plugins: [react(), tailwindcss(), basicSsl(),],
+//     server: {
+//         host: true,
+//         https: true,
+//     },
+//     // define: {
+//     //     APP_VERSION: JSON.stringify(process.env.npm_package_version),   // Create a variable tp store project version from package.json file
+//     // },
+// })
+
+
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// import mkcert from 'vite-plugin-mkcert'
 import basicSsl from '@vitejs/plugin-basic-ssl'
-// import pkg from './package.json' with {type: "json"}
 
 export default defineConfig({
-    plugins: [react(), tailwindcss(), basicSsl(),],
+    plugins: [
+        react(),
+        tailwindcss(),
+        process.env.NODE_ENV === 'development' && basicSsl(), // Conditionally include basicSsl()
+    ].filter(Boolean), // Filter out `false` values if basicSsl() is not included
     server: {
         host: true,
         https: true,
     },
-    // define: {
-    //     APP_VERSION: JSON.stringify(process.env.npm_package_version),   // Create a variable tp store project version from package.json file
-    // },
 })
